370e7d0751c3163274a3c50464b16a9b390db8a2
Tek <tek@inbox.ru>
2015-08-23 21:17:49 +0400

    new build

--- dist/tek.progress-bar.js
+++ dist/tek.progress-bar.js
@@ -12,7 +12,8 @@
                 manager: "=",
                 containerClass: "@class",
                 barClass: "@",
-                ngModel: "="
+                ngModel: "=",
+                mode: '@'
             },
             restrict: "E",
             transclude: true,
@@ -24,12 +25,16 @@
 
                 var settings = {
                     fullClass: 'full-bar',
-                    emptyClass: 'empty-bar'
+                    emptyClass: 'empty-bar',
+                    verticalClass: 'vertical',
+                    horizontalClass: ''
                 };
 
-                function ProgressObj(element) {
+                function ProgressObj(element, mode) {
                     var divElements = element.find('div');
+                    this.mode = mode;
                     this.containerElement = angular.element(divElements[0]);
+                    this.containerElement.addClass(settings[mode + 'Class']);
                     this.barContainer = angular.element(divElements[1]);
                     this.value = 0;
                 }
@@ -40,17 +45,23 @@
 
                 ProgressObj.prototype.set = function (val) {
                     this.value = val;
-                    this.barContainer.css('width', val + '%');
+                    if(this.mode === 'horizontal'){
+                        this.barContainer.css('width', val + '%');
+                    }
+                    if(this.mode === 'vertical'){
+                        this.barContainer.css('height', val + '%');
+                    }
                     this.updateClasses();
                 };
 
                 ProgressObj.prototype.updateClasses = function () {
-                    if (this.value === 0) {
+                    console.log(this.value);
+                    if (this.value <= 0) {
                         this.containerElement.removeClass(settings.fullClass);
                         return this.containerElement.addClass(settings.emptyClass);
                     }
 
-                    if (this.value === 100) {
+                    if (this.value >= 100) {
                         this.containerElement.removeClass(settings.emptyClass);
                         return this.containerElement.addClass(settings.fullClass);
                     }
@@ -64,8 +75,8 @@
                 };
 
                 bar.init = function () {
-                    bar.progressObj = new ProgressObj($element);
-
+                    bar.mode = (bar.mode === 'vertical') ? bar.mode : 'horizontal';
+                    bar.progressObj = new ProgressObj($element, bar.mode);
                     var facade = {
                         get: function () {
                             return bar.progressObj.get();
@@ -92,7 +103,15 @@
                     }
 
                     if (bar.ngModel !== undefined) {
-                        $scope.$watch('bar.ngModel', function (newVal, oldVal) {
+                        $scope.$watch('bar.ngModel', function (newVal) {
+                            if(typeof newVal !== 'number' || newVal < 0 || newVal !== newVal){
+                                newVal = 0;
+                            }
+
+                            if(newVal > 100){
+                                newVal = 100;
+                            }
+
                             if (bar.manager) {
                                 bar.manager._updateValue(newVal);
                             }
